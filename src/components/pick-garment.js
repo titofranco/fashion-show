44e2f8c4b1cb63cad5ce973ca6ca92305f71cbab
Carlos Guisao Franco <carlos.guisao@globant.com>
2022-12-02 11:50:04 -0500

    Customize Prev and Next Slick arrows

--- src/components/pick-garment.js
+++ src/components/pick-garment.js
@@ -1,10 +1,47 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import Slider from "react-slick";
-import { carouselSettings} from "../utils/misc"
 import '../App.css';
 import { PickGarmentContainer, Garment, PriceTag } from "./lib";
 import styles from '../shared.module.css'
+import prevArrow from '../images/prevArrow.png'
+import nextArrow from '../images/nextArrow.png'
+
+const carouselSettings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  prevArrow: <SlickPrevArrow source={prevArrow}/>,
+  nextArrow: <SlickNextArrow source={nextArrow}/>
+};
+
+function SlickNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <img
+      src={nextArrow}
+      alt="Next Arrow"
+      className={className}
+      style={{ ...style, display: "block"}}
+      onClick={onClick}
+    />
+  );
+}
+
+function SlickPrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <img
+      src={prevArrow}
+      alt="Prev Arrow"
+      className={className}
+      style={{ ...style, display: "block"}}
+      onClick={onClick}
+    />
+  );
+}
 
 export default function PickGarment({ garmentList, option, variant }) {
   return (
--- src/utils/misc.js
+++ src/utils/misc.js
@@ -3,14 +3,6 @@ import { Gem, GoldCoin } from '../components/lib'
 const gemRandomPrice = () => Math.floor(Math.random() * 1000)
 const goldRandomPrice = () => Math.floor(Math.random() * 100) / 10 + ' K'
 
-const carouselSettings = {
-  dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 2
-};
-
 function garmentObject(image) {
   const index = Math.floor(Math.random() * 2)
   return {
@@ -20,4 +12,4 @@ function garmentObject(image) {
   }
 }
 
-export {carouselSettings, garmentObject}
+export {garmentObject}
